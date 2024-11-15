e43e92097cedec529b23af3d6d221639d0c38403
ISU <ourwavelets@gmail.com>
2024-11-15 12:04:06 +0900

    feat: Add FoodCategory entity

---
+++ src/main/java/demo/order/domain/FoodCategory.java
@@ -0,0 +1,26 @@
+package demo.order.domain;
+
+import demo.order.domain.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
